c9c456beff0e7f29ec3ebb0ab66d3f8b5a17175f
Sigurbjörg Erla <sepbiering@gmail.com>
2025-02-28 22:35:28 +0000

    Made the recipe overview

--- app/src/main/java/hbv601g/recipeapp/ui/recipes/RecipesFragment.java
+++ app/src/main/java/hbv601g/recipeapp/ui/recipes/RecipesFragment.java
@@ -1,27 +1,79 @@
 package hbv601g.recipeapp.ui.recipes;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import hbv601g.recipeapp.MainActivity;
+import hbv601g.recipeapp.R;
+import hbv601g.recipeapp.adapters.RecipeAdapter;
 import hbv601g.recipeapp.databinding.FragmentRecipesBinding;
+import hbv601g.recipeapp.entities.Recipe;
+import hbv601g.recipeapp.networking.NetworkingService;
+import hbv601g.recipeapp.service.RecipeService;
 
 public class RecipesFragment extends Fragment {
 
     private FragmentRecipesBinding binding;
+    private RecipeService mRecipeService;
+    private List<Recipe> mRecipeList;
+
+    private ListView mRecipeListView;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
-
         binding = FragmentRecipesBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        return root;
+        MainActivity mainActivity = (MainActivity) getActivity();
+        assert mainActivity != null;
+
+        NavController navController = Navigation.findNavController(mainActivity, R.id.nav_host_fragment_activity_main);
+
+        long uid = mainActivity.getUserId();
+        mRecipeService = new RecipeService(new NetworkingService(), uid);
+
+
+        try{
+            mRecipeList = mRecipeService.getAllRecipes();
+        } catch (NullPointerException e){
+            mRecipeList = new ArrayList<>();
+            mainActivity.makeToast(R.string.get_recipes_failed_toast, Toast.LENGTH_LONG);
+        }
+
+        mRecipeListView = binding.recipesListView;
+
+        // Gera adapter til að tengja lista af ingredients við listView hlutinn
+        RecipeAdapter recipeAdapter = new RecipeAdapter(mainActivity.getApplicationContext(), mRecipeList);
+        mRecipeListView.setAdapter(recipeAdapter);
+
+        mRecipeListView.setOnItemClickListener((parent, view, position, id) -> {
+            Recipe ingredient = (Recipe) parent.getItemAtPosition(position);
+            Log.d("Selected", ingredient.toString());
+
+            Bundle bundle = new Bundle();
+            bundle.putParcelable(getString(R.string.selected_recipe), ingredient);
+
+            //navController.navigate(R.id.navigation_ingredient, bundle);
+            //Log.d("Selected", "ingredient by " + ingredient.getCreatedBy().getUsername());
+
+        });
+
+
+        return  root;
     }
 
     @Override
