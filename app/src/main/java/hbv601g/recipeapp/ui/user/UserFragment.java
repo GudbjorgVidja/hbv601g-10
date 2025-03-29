3df5114ce70542c4062c9f8070e99e5a13852be6
Sigurbjörg Erla <sepbiering@gmail.com>
2025-03-29 23:53:14 +0000

    Fixed check for which profile to show, added documentation

--- app/src/main/java/hbv601g/recipeapp/ui/user/UserFragment.java
+++ app/src/main/java/hbv601g/recipeapp/ui/user/UserFragment.java
@@ -51,7 +51,6 @@ public class UserFragment extends Fragment{
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        // Gæti verið betra að hafa user sem argument, og ef það er ekki til staðar þá enginn user
         MainActivity mainActivity = (MainActivity) getActivity();
         assert mainActivity != null;
         NavController navController = Navigation.findNavController(mainActivity, R.id.nav_host_fragment_activity_main);
@@ -61,7 +60,7 @@ public class UserFragment extends Fragment{
 
         mRecipeListService = new RecipeListService(new NetworkingService(), mainActivity.getUserId());
 
-        if (getArguments() != null) {
+        if (getArguments() != null && !getArguments().isEmpty()) {
             mUidOfProfile = getArguments().getLong(getString(R.string.selected_user_id));
             mNameOfProfile = getArguments().getString(getString(R.string.selected_user_name));
         }
@@ -70,17 +69,21 @@ public class UserFragment extends Fragment{
             mNameOfProfile = mainActivity.getUserName();
         }
 
-
-
-        if(mainActivity.getUserId() != 0) setWithUserView(mainActivity,navController);
-        else setNoUserView(mainActivity,navController);
-
+        if (mainActivity.getUserId() == 0 && mUidOfProfile == 0){
+            setLoginView(navController);
+        }
+        else setWithUserView(mainActivity, navController);
 
         return root;
     }
 
-    private void setNoUserView(MainActivity mainActivity, NavController navController){
-        // Set visibility of UI components
+
+    /**
+     * sets the visibility of UI components to display the login and signup page,
+     * and adds listeners to buttons.
+     * @param navController the NavController used to navigate between fragments
+     */
+    private void setLoginView(NavController navController){
         mBinding.usernameDisplay.setVisibility(GONE);
         mBinding.createRecipeListButton.setVisibility(GONE);
         mBinding.logoutButton.setVisibility(GONE);
@@ -91,13 +94,18 @@ public class UserFragment extends Fragment{
         mBinding.loginButton.setVisibility(VISIBLE);
         mBinding.signupButton.setVisibility(VISIBLE);
 
-        // Set button listeners
         mBinding.loginButton.setOnClickListener(v -> navController.navigate(R.id.nav_login));
         mBinding.signupButton.setOnClickListener(v -> navController.navigate(R.id.nav_signup));
     }
 
+    /**
+     * Sets the visibility of UI components to display a user profile (as opposed to displaying
+     * the login/signup page). Displays the username connected to the displayed profile, 
+     * populates the list of recipe lists and displays them. Adds relevant listeners
+     * @param mainActivity the current activity
+     * @param navController the NavController used to navigate between fragments
+     */
     private void setWithUserView(MainActivity mainActivity, NavController navController){
-        // Set visibility of UI components
         mBinding.usernameDisplay.setVisibility(VISIBLE);
         mBinding.userRecipeListSection.setVisibility(VISIBLE);
         mBinding.userRecipeLists.setVisibility(VISIBLE);
@@ -105,59 +113,69 @@ public class UserFragment extends Fragment{
         mBinding.loginButton.setVisibility(GONE);
         mBinding.signupButton.setVisibility(GONE);
 
-        // Populate the list of recipe lists and display them
+        mBinding.usernameDisplay.setText(mNameOfProfile);
+
+        populateRecipeListOverview(mainActivity,navController);
+        
+        if(mUidOfProfile == mainActivity.getUserId() || mUidOfProfile==0){
+            displayOwnProfile(mainActivity,navController);
+        }
+        else displayOtherUserProfile();
+    }
+
+    /**
+     * Populates the list of recipe lists and displays them. Adds a listener to react to 
+     * list items being selected
+     * @param mainActivity the current activity
+     * @param navController the NavController used to navigate between fragments
+     */
+    private void populateRecipeListOverview(MainActivity mainActivity, NavController navController) {
         try {
-            mRecipeLists = mRecipeListService.getUserRecipeLists(mainActivity.getUserId());
+            mRecipeLists = mRecipeListService.getUserRecipeLists(mUidOfProfile);
         } catch(NullPointerException e) {
             mRecipeLists = new ArrayList<>();
             mainActivity.makeToast(R.string.null_recipe_lists, Toast.LENGTH_LONG);
         }
-
+        
         mRecipeListListView = mBinding.userRecipeLists;
         RecipeListAdapter recipeListAdapter = new RecipeListAdapter(mainActivity.getApplicationContext(), mRecipeLists);
         mRecipeListListView.setAdapter(recipeListAdapter);
 
-        // Set the on click listener for the recipe list list view
         mRecipeListListView.setOnItemClickListener((parent, view, position, id) -> {
             RecipeList recipeList = (RecipeList) parent.getItemAtPosition(position);
             Bundle bundle = new Bundle();
             bundle.putParcelable(getString(R.string.selected_recipe_list), recipeList);
             navController.navigate(R.id.nav_recipe_list, bundle);
         });
-
-        if(mUidOfProfile == mainActivity.getUserId() || mUidOfProfile==0){
-            ownProfile(mainActivity,navController);
-        }
-        else setOtherProfile(mainActivity,navController);
     }
 
-    private void ownProfile(MainActivity mainActivity, NavController navController){
-        // Set visibility of UI components
+    /**
+     * Sets the visibility of UI components specific to users viewing their own profiles 
+     * (but not when users are viewing profiles of other users). Sets listeners specific
+     * to the same situation.
+     *
+     * @param mainActivity the current activity
+     * @param navController the NavController used to navigate between fragments
+     */
+    private void displayOwnProfile(MainActivity mainActivity, NavController navController){
         mBinding.createRecipeListButton.setVisibility(VISIBLE);
         mBinding.logoutButton.setVisibility(VISIBLE);
         mBinding.deleteUserButton.setVisibility(VISIBLE);
 
-
-        // Set the username
-        mBinding.usernameDisplay.setText(mainActivity.getUserName());
-
-        // Set listeners on buttons
         mBinding.logoutButton.setOnClickListener(v -> mainActivity.removeCurrentUser());
-
+        mBinding.deleteUserButton.setOnClickListener(v -> deleteUserAlert(mainActivity));
         mBinding.createRecipeListButton.setOnClickListener(
                 v -> navController.navigate(R.id.navigation_new_recipe_list));
-
-        mBinding.deleteUserButton.setOnClickListener(v -> deleteUserAlert(mainActivity));
     }
 
-    private void setOtherProfile(MainActivity mainActivity, NavController navController){
-        // Set visibility of UI components
+    /**
+     * Sets the visibility of UI components specific to users viewing the profile of another
+     * user (but not when users are viewing their own profiles)
+     */
+    private void displayOtherUserProfile(){
         mBinding.createRecipeListButton.setVisibility(GONE);
         mBinding.logoutButton.setVisibility(GONE);
         mBinding.deleteUserButton.setVisibility(GONE);
-
-        // Set the username
-        mBinding.usernameDisplay.setText(mNameOfProfile);
     }
 
 
